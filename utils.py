051862c69223045947e15a7a1c202a39c143ee7b
Kevin in AS <kevin820606@gmail.com>
2023-12-05 17:07:59 +0800

    fail in day 5

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-
-def read_quiz(day: int):
-    with open(f"data/day{day}.txt", mode= 'r') as readfile:
+def read_quiz(day: int, is_example: bool = False):
+    example_file = "_example" if is_example else ""
+    file_path = f"data/day{day}{example_file}.txt"
+    with open(file_path, mode="r") as readfile:
         return readfile.readlines()
