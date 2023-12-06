475d68b68c51de2e16cb4c7e20ce0a77fd2a77bd
Kevin in AS <kevin820606@gmail.com>
2023-12-06 17:46:13 +0800

    fix R code

--- day6.R
+++ day6.R
@@ -1,4 +1,4 @@
-library(tidyverse)
+library(bit64)
 time <- c(7, 15, 30)
 distance <- c(9, 40, 200)
 
@@ -7,11 +7,11 @@ distance <- c(214, 1789, 1127, 1055)
 
 calc_dist <- function(time, dist) {
     sum(
-        ((log(0:time) + log(rev(0:time)))) > log(dist)
+        as.integer64(0):time * rev(as.integer64(0):time) > dist
     )
 }
 print(mapply(calc_dist, time, distance))
 print(calc_dist(
-    as.numeric(paste0(time, collapse = "")),
-    as.numeric(paste0(distance, collapse = ""))
+    as.integer64(paste0(time, collapse = "")),
+    as.integer64(paste0(distance, collapse = ""))
 ))
